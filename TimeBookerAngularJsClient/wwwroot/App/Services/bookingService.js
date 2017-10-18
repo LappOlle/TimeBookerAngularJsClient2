88f5e8063c83366b9f851792a1a3ed5da1cff8e2
Andreas Tangjai <skogsmulle87@gmail.com>
2017-10-18 16:53:06 +0200

    Add project files.

---
+++ TimeBookerAngularJsClient/wwwroot/App/Services/bookingService.js
@@ -0,0 +1,16 @@
+﻿'use strict';
+app.factory('bookingService', ['$http', function ($http) {
+
+    var serviceBase = 'http://localhost:57904/';
+    var bookingServiceFactory = {};
+    var _getBookings = function () {
+        return $http.get(serviceBase + 'api/Booking').then(function (results) {
+            return results;
+        });
+    };
+
+    bookingServiceFactory.getBookings = _getBookings;
+
+    return bookingServiceFactory;
+
+}]);
---
+++ TimeBookerAngularJsClient/wwwroot/App/app.js
@@ -0,0 +1,34 @@
+﻿var app = angular.module('AngularAuthApp', ['ngRoute', 'LocalStorageModule', 'angular-loading-bar', 'ui.calendar']);
+
+app.config(function ($httpProvider) {
+    $httpProvider.interceptors.push('authInterceptorService');
+});
+
+app.config(function ($routeProvider) {
+
+    $routeProvider.when("/home", {
+        controller: "homeController",
+        templateUrl: "/App/Views/home.html"
+    });
+
+    $routeProvider.when("/login", {
+        controller: "loginController",
+        templateUrl: "/App/Views/login.html"
+    });
+
+    $routeProvider.when("/signup", {
+        controller: "signupController",
+        templateUrl: "/App/Views/signup.html"
+    });
+
+    $routeProvider.when("/booking", {
+        controller: "bookingController",
+        templateUrl: "/App/Views/booking.html"
+    });
+
+    $routeProvider.otherwise({ redirectTo: "/home" });
+});
+
+app.run(['authService', function (authService) {
+    authService.fillAuthData();
+}]);
